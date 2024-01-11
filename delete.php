41f5734dde0bed047cf5d56e2b736d151e2e1a82
abderrahmane0402 <abderrahmanesabkari@gmail.com>
2024-01-11 16:29:27 +0100

    delete method

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'load.php';
+use BaseXClient\BaseXException;
+use BaseXClient\Session;
+
+try {
+    // create session
+    $session = new Session("localhost", 1984, "admin", "admin");
+
+    // perform command and print returned string
+    $session->execute("OPEN database");
+    print $session->info();
+
+    $query = $session->execute("xquery delete node //Concours/test");
+
+    // close session
+    $session->close();
+
+} catch (BaseXException $e) {
+    // print exception
+    print $e->getMessage();
+}
+
